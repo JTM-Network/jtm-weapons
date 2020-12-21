e10a0a2c1df9dcf3cc5f2692880d07b8376c48bd
Joe Matthews <joetymatthews@gmail.com>
2020-12-21 16:16:34 +0000

    New: Initial Project files.

---
+++ src/main/java/com/jtmnetwork/mc/Weapons.java
@@ -0,0 +1,22 @@
+package com.jtmnetwork.mc;
+
+import com.jtmnetwork.framework.Framework;
+
+public class Weapons extends Framework {
+
+    public void registerUis() {
+
+    }
+
+    public void registerModules() {
+
+    }
+
+    public void enable() {
+
+    }
+
+    public void disable() {
+
+    }
+}
